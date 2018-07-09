39af9b93f6bf177f201aad8b117391af942e719b
David Sancho <dsnxmoreno@gmail.com>
2018-07-09 08:26:25 +0200

    Include package-lock into docker container

--- ops/run.sh
+++ ops/run.sh
@@ -1,9 +1,9 @@
 # docker network create intranet
 
-# docker run -d \
-#   --name watchtower \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#   v2tec/watchtower
+docker run -d \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  v2tec/watchtower
 
 # docker run -d \
 #   --name proxy \
